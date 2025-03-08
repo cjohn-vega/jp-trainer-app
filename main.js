db8f79ccb0fc661d9227895b977cbdb0cd0634be
Chris Vega <vegacoder.az@gmail.com>
2025-03-08 16:48:34 -0700

    Added randomized music player

--- main.js
+++ main.js
@@ -10,6 +10,30 @@ let makeContainerID = (idName) => {
   return elem;
 };
 
+let makeText = (elementType, innerText, className) => {
+  let element = document.createElement(elementType);
+  let text = document.createTextNode(innerText);
+  element.appendChild(text);
+  if (className !== undefined) {
+    element.className = className;
+  }
+  return element;
+};
+
+let makeAnchor = (innerText, href, className, setTarget) => {
+  let aTag = document.createElement("a");
+  aTag.href = href;
+  if (className !== undefined) {
+    aTag.className = className;
+  }
+  if (setTarget !== undefined) {
+    aTag.target = setTarget;
+  }
+  let text = document.createTextNode(innerText);
+  loadElement(text, aTag);
+  return aTag;
+};
+
 let makeNavBar = (logoText, linksArray) => {
   let nav = document.createElement("nav");
   let navContainer = makeContainerClass("navbar-container");
@@ -40,14 +64,38 @@ let makeNavBar = (logoText, linksArray) => {
   return nav;
 };
 
-let makeText = (elementType, innerText, className) => {
-  let element = document.createElement(elementType);
-  let text = document.createTextNode(innerText);
-  element.appendChild(text);
-  if (className !== undefined) {
-    element.className = className;
-  }
-  return element;
+let getRandomItem = (arr) => {
+  // Ensure the array is not empty
+  if (arr.length === 0) return undefined;
+
+  // Generate a random index based on the array length
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  // Return the item at the random index
+  return arr[randomIndex];
+};
+
+let makeAudioPlayer = (musicArray) => {
+  let randomSong = getRandomItem(musicArray);
+  let audioContainer = makeContainerClass("audio-player");
+  let audio = document.createElement("audio");
+  audio.id = "audio";
+  audio.controls = true;
+  audio.autoplay = true;
+  let source = document.createElement("source");
+  source.src = randomSong.directory;
+  source.type = "audio/mp3";
+  loadElement(source, audio);
+  let textNode = document.createTextNode(
+    "Your browser does not support this audio."
+  );
+  loadElement(textNode, audio);
+  loadElement(audio, audioContainer);
+  loadElement(
+    makeAnchor(`${randomSong.song}🔗`, randomSong.href, "song-title", "_blank"),
+    audioContainer
+  );
+  return audioContainer;
 };
 
 let loadBody = (element) => {
@@ -69,6 +117,15 @@ let navLinks = [
   { text: "Contact", href: "#" },
 ];
 
+let music = [
+  {
+    directory: "assets/music/sleepwalker.mp3",
+    song: "ILLENIUM - Sleepwalker ft. Joni Fatora",
+    href: "https://www.youtube.com/watch?v=lhVykHuCpyo",
+  },
+];
+
 loadBody(makeNavBar("JP Trainer", navLinks));
 loadBody(makeContainerID("main"));
+loadElementID(makeAudioPlayer(music), "main");
 loadElementID(makeText("h1", "jp trainer", "title"), "main");
